d103ccd4d1c3ce38ff4cbc673427076234ec84c4
Eugene Leonkov <eugene.leonkov@soft-fx.lv>
2019-07-30 13:30:13 +0300

    add service method

--- src/services/fxopen-service.js
+++ src/services/fxopen-service.js
@@ -1,5 +1,22 @@
 class FXOpenService {
 
+    _apiBase = 'https://cryptottlivewebapi.fxopen.net:8443/';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    async getAvailablePublicSymbols() {
+        const res = await this.getResource('/api/v1/public/symbol/');
+
+        return res;
+    }
 }
 
 export default FXOpenService;
