ef02a10903d43c0e133235cf2300746db0483e8d
Eugene Leonkov <eugene.leonkov@soft-fx.lv>
2019-07-30 16:30:18 +0300

    currency list updated

--- src/components/currency-list-item/currency-list-item.js
+++ src/components/currency-list-item/currency-list-item.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+import './currency-list-item.css'
+
+import btc from '../../assets/images/currency/btc.svg';
+import ltc from '../../assets/images/currency/ltc.svg';
+import dsh from '../../assets/images/currency/dsh.svg';
+import usd from '../../assets/images/currency/usd.svg';
+
+const currency = {
+    btc, ltc, dsh, usd
+};
+
 class CurrencyListItem extends Component {
     render = () => {
+        const { symbol, marginCurrencyName, profitCurrencyName } = this.props;
+
         return (
-            <div>CurrencyListItem</div>
+            <div className="currency-list-item">
+                <div className="currency-list-item__logo">
+                    <img src={currency[marginCurrencyName.toLowerCase()]} />
+                    <img src={currency[profitCurrencyName.toLowerCase()]} />
+                </div>
+                <div className="currency-list-item__text">{symbol}</div>
+            </div>
         );
     };
 }
