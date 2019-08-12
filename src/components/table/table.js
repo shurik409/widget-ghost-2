71154aa126abef049a383297116de1a7bb20f21d
Vladislav Goman <vladislav.goman@soft-fx.lv>
2019-08-12 18:11:27 +0300

    Table component totally  optimized.

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -11,34 +11,27 @@ const Table = ({lastBidAsk}) => {
     const [{ Asks, Bids, BestBid, BestAsk }] = lastBidAsk;
 
     return (
-        <>
-            {
-                <table className='col-md-12'>
-                    <tbody>
-                        {
-                            Bids.map((data, index) => 
-                                <tr key={index} className='info-row'>
-                                    <td>{data.Volume}</td>
-                                    <td>{data.Price}</td>
-                                    <td>{Asks[index].Price}</td>
-                                    <td>{Asks[index].Volume}</td>
-                                </tr>
-                            )
-                        }
-                        <tr className='bold-row'>
-                            <td>{BestBid.Volume}</td>
-                            <td>{BestBid.Price}</td>
-                            <td>{BestAsk.Price}</td>
-                            <td>{BestAsk.Volume}</td>
+        <table className='col-md-12'>
+            <tbody>
+                {
+                    Bids.map((data, index) => 
+                        <tr key={index} className='info-row'>
+                            <td>{data.Volume}</td>
+                            <td>{data.Price}</td>
+                            <td>{Asks[index].Price}</td>
+                            <td>{Asks[index].Volume}</td>
                         </tr>
-                    </tbody>
-                </table>
-            }
-        </>
+                    )
+                }
+                <tr className='bold-row'>
+                    <td>{BestBid.Volume}</td>
+                    <td>{BestBid.Price}</td>
+                    <td>{BestAsk.Price}</td>
+                    <td>{BestAsk.Volume}</td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
-export {
-    Table
-};
 
 export default withCryptoService()(Table);
